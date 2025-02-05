5dd1d21c5465ca2acf519e2f40be2f98dd1f332b
Malik Öztürk <malikozturk975@gmail.com>
2025-02-05 03:13:57 +0300

    fix: add title in services page

--- src/app/hizmetler/page.tsx
+++ src/app/hizmetler/page.tsx
@@ -6,24 +6,26 @@ import { services, IService, slugify } from "@/data/services";
 
 export default async function Services() {
     return (
-        <div className="p-4 pb-8 mx-auto max-w-7xl grid gap-4 sm:p-6 lg:p-8 md:pb-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-            {services && (services as IService[]).map((service: IService) => (
-                <Link
-                    href={`hizmetler/${slugify(service.title)}`}
-                    key={slugify(service.title)}
-                    className="flex flex-col items-center gap-3 p-4 rounded-lg bg-white justify-end"
-                >
-                    <img
-                        src={`/images/services/${slugify(service.title)}.jpg`}
-                        alt={service.title}
-                        className="rounded-lg mb-3"
-                    />
-                    <h3 className="font-bold text-xl truncate w-full">{service.title}</h3>
-                    <p className="font-medium text-base text-[#687279] truncate w-full">{service.description}</p>
-                    <button type="button" className="w-full inline-flex items-center justify-center px-5 py-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Devamını Gör</button>
-                </Link>
-            ))}
+        <div className="flex flex-col gap-4 p-4 pb-8 mx-auto max-w-7xl sm:p-6 lg:p-8 md:pb-16">
+            <h1 className="font-bold text-4xl">Hizmetlerimiz</h1>
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+                {services && (services as IService[]).map((service: IService) => (
+                    <Link
+                        href={`hizmetler/${slugify(service.title)}`}
+                        key={slugify(service.title)}
+                        className="flex flex-col items-center gap-3 p-4 rounded-lg bg-white justify-end"
+                    >
+                        <img
+                            src={`/images/services/${slugify(service.title)}.jpg`}
+                            alt={service.title}
+                            className="rounded-lg mb-3"
+                        />
+                        <h3 className="font-bold text-xl truncate w-full">{service.title}</h3>
+                        <p className="font-medium text-base text-[#687279] truncate w-full">{service.description}</p>
+                        <button type="button" className="w-full inline-flex items-center justify-center px-5 py-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Devamını Gör</button>
+                    </Link>
+                ))}
+            </div>
         </div>
-
     )
 }
