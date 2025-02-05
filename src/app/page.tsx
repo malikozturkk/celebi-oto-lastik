594c10dafe15961e245b4093ddb3a1d559dad32c
Malik Öztürk <malikozturk975@gmail.com>
2025-02-06 01:05:56 +0300

    feat: add swiper package and brands in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import { homeMetadata } from '@/metadata/homeMetadata';
 import { Metadata } from "next";
+import Brands from "@/components/Brands";
 
 export const metadata: Metadata = homeMetadata;
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        sasa
-      </main>
+    <div className="flex flex-col gap-4 p-4 pb-8 mx-auto max-w-7xl sm:p-6 lg:p-8 md:pb-16">
+      <h2 className="text-xl font-bold">Markalar</h2>
+      <Brands />
     </div>
   );
 }
---
+++ src/components/Brands/index.tsx
@@ -0,0 +1,35 @@
+"use client"
+import React from "react"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { Autoplay, FreeMode } from 'swiper/modules';
+
+const brands = ["petlas", "bridgestone", "continental", "goodyear", "hankook", "laufenn", "michelin", "milestone", "pirelli"]
+
+const Brands = () => {
+    return (
+        <div className="w-full h-40 flex bg-white rounded-lg max-h-28">
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                freeMode={true}
+                loop
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[FreeMode, Autoplay]}
+                autoplay={{ delay: 1000, disableOnInteraction: false }}
+            >
+                {brands.map((brand) => (
+                    <SwiperSlide key={brand} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <img src={`/images/brands/${brand}.png`} className="w-28 max-h-24" alt={brand} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    )
+}
+
+export default Brands
