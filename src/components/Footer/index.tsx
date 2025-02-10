69f234d8801afee4f6fa80e1daa7d6768a5ef4ca
Malik Öztürk <malikozturk975@gmail.com>
2025-02-10 18:24:03 +0300

    fix: remove facebook item

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,14 +21,6 @@ const Footer = () => {
                             </li>
 
                             <li>
-                                <Link href="https://www.facebook.com/celebiotolastik45/" target="_blank" title="Çelebi Oto Lastik - Facebook" className="flex items-center justify-center text-white transition-all duration-200 bg-[#1f2937] rounded-full w-7 h-7 hover:bg-red focus:bg-red">
-                                    <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                        <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                                    </svg>
-                                </Link>
-                            </li>
-
-                            <li>
                                 <Link href="https://www.instagram.com/celebijantlastikk/" target="_blank" title="Çelebi Oto Lastik - Instagram" className="flex items-center justify-center text-white transition-all duration-200 bg-[#1f2937] rounded-full w-7 h-7 hover:bg-red focus:bg-red">
                                     <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                         <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path>
