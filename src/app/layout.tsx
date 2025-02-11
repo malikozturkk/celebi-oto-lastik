cc954634538f6d47cd0ed841d2b4e7eaecf418c3
Malik Öztürk <malikozturk975@gmail.com>
2025-02-11 11:46:09 +0300

    feat: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Whatsapp from "@/components/Whatsapp";
 import Header from "@/components/Header";
+import { GoogleAnalytics, GoogleTagManagerNoScript } from "@/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f6f6f6]`}
       >
+        <GoogleTagManagerNoScript />
         <Header />
         <Navbar />
         {children}
